2eaa59b846fb7118d1d3bd9ae46e44b0ad176629
ronikjoshi <ronikjoshi98@gmail.com>
2025-11-16 14:12:37 +0000

    creates models

--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit:"30mb", extended:true }));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended:true }));
 
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://ronikjoshi:ronikjoshi123@cluster0.t9alssw.mongodb.net/?appName=Cluster0";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(() => PORT, console.log(`Server running on port: ${PORT}`)))
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
